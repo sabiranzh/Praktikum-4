f6267fe4062524dd41df429fb8ff5ff1672a7a64
sabiranzh <146188896+sabiranzh@users.noreply.github.com>
2024-03-20 21:38:07 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.1.0
+    </div>
+    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">by Sabira Najma Khalizah</a>.</strong> All rights reserved.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="Template/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="Template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="Template/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="Template/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="Template/dist/js/demo.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+
+<?php
+require_once 'header.php';
+require_once 'sidebar.php';
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Dashborad</h1>
+          <h3 class = mt-4>Selamat datang dihalaman admin</h3>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+
+  <!-- Main content -->
+  <section class="content">
+
+    
+            <!-- /.card-footer-->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+require_once 'footer.php';
+?>
